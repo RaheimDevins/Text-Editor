a2a7d120d559e4c7e8d966c841579a897d2f4de0
RaheimDevins <raheimdev2011@gmail.com>
2023-10-02 15:08:22 -0500

    refactored src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,5 +28,15 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        status: [0, 200],
+      }),
+    ],
+  })
 );
+
+offlineFallback();
